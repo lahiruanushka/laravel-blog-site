2eb26b42cba59b2ab51da79b85b3f4a945e46079
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-07-13 16:16:16 +0530

    Update frontend.blade.php

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -26,19 +26,12 @@
     <header class="blog-header py-3">
         <div class="container">
             <div class="row flex-nowrap justify-content-between align-items-center">
-                <div class="col-4 text-center">
+                <div class="col-4 text-left">
                     <a class="blog-header-logo text-dark" href="#" style="text-decoration: none;">BlogNest</a>
                 </div>
                 <div class="col-4 d-flex justify-content-end align-items-center">
-                    <a class="link-secondary" href="#" aria-label="Search">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img" viewBox="0 0 24 24">
-                            <title>Search</title>
-                            <circle cx="10.5" cy="10.5" r="7.5" />
-                            <path d="M21 21l-5.2-5.2" />
-                        </svg>
-                    </a>
-                    <a class="btn btn-sm btn-outline-secondary ml-2" href="{{ route('register') }}">Sign up</a>
-                    <a class="btn btn-sm btn-outline-primary mr-4" href="{{ route('login') }}">Login</a>
+                    <a class="btn btn-sm btn-outline-secondary m-2" href="{{ route('register') }}">Sign up</a>
+                    <a class="btn btn-sm btn-outline-primary m-2" href="{{ route('login') }}">Login</a>
                 </div>
             </div>
         </div>
