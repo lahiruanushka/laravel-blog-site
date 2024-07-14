5b6267d6aa12515cf8c7b54b2d7d37aa47b2eaa3
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-07-14 19:59:36 +0530

    Add route groups

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,25 @@ Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-// Posts
-Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
 Route::get('/posts/show/{id}', [PostController::class, 'show'])->name('posts.show');
+
+// Posts
+Route::group(['middleware' => 'auth'], function(){
 Route::get('/posts/all', [HomeController::class, 'allPosts'])->name('posts.all');
+Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
 Route::get('/posts/edit/{postId}', [PostController::class, 'edit'])->name('posts.edit');
 Route::post('/posts/update/{postId}', [PostController::class, 'update'])->name('posts.update');
 Route::get('/posts/delete/{postId}', [PostController::class, 'delete'])->name('posts.delete');
+});
+
 
 // Admin Routes
-Route::get('/admin/dashboard', [DashboardController::class, 'index'])->middleware('admin')->name('admin.dashboard');
+// Route::group('middleware' => ['admin', 'auth'], function(){
+// 	Route::get('/admin/dashboard', [DashboardController::class, 'index'])->middleware('admin')->name('admin.dashboard');
+
+// });
+
+Route::group(['middleware' => ['admin'], 'prfix' => 'admin','as' => 'admin.'],function(){
+		Route::get('/admin/dashboard', [DashboardController::class, 'index'])->middleware('admin')->name('dashboard');
+
+})
