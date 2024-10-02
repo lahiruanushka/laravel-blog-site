284ac8ee2203f66e3960b3e2978a6a91c85947fa
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-10-02 12:04:34 +0530

    Update WelcomeController.php

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,14 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Post; // Ensure you have imported the Post model
+use App\Models\Post;
 
 class WelcomeController extends Controller
 {
   public function index()
-{
+  {
     $posts = Post::latest()->take(6)->get();
     return view('welcome', compact('posts'));
-}
-
+  }
 }
